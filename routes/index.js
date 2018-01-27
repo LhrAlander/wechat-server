f8465f816b283caad70d2860ca3d811a63a4c55e
LhrAlander <1147816814@qq.com>
2018-01-27 16:07:24 +0800

    微信测试号后台服务

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.query)
+  res.send(req.query.echostr)
+});
+
+module.exports = router;
