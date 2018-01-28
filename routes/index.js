e53e0c7c6ed93eff8d11f451c88e6926b7625039
LhrAlander <1147816814@qq.com>
2018-01-28 10:54:23 +0800

    回复消息测试

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const xmlparser = require('express-xml-bodyparser')
 
 /* GET home page. */
-router.get('/*', xmlparser({trim: false, explicitArray:false}), function(req, res, next) {
-  console.log(req.body)
-  res.send('hello')
+router.get('/', (req, res, next) => {
+  res.send(req.query.echostr)
+})
+router.post('/*', xmlparser({trim: false, explicitArray:false}), function(req, res, next) {
+  let data = `<xml><ToUserName>${req.body.xml.fromusername}</ToUserName> <FromUserName>${req.body.xml.tousername}</FromUserName> <CreateTime>${req.body.xml.createtime}</CreateTime> <MsgType>${req.body.xml.msgtype}</MsgType> <Content>'你好啊！'</Content></xml>`
+  res.writeHead(200, {'Content-Type': 'application/xml'});
+  res.end(data);
 });
 
 module.exports = router;
